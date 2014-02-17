c14c9d424d9881d807ab7ee937b9a4790c8aaff8
Steve Chakif <schakif@Steves-MacBook-Pro-2.local>
2014-02-16 22:45:11 -0500

    Initial Commit

---
+++ Practice1/Practice1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Practice1AppDelegate.h
+//  Practice1
+//
+//  Created by Steve Chakif on 2/16/14.
+//  Copyright (c) 2014 Steve Chakif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Practice1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Practice1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Practice1
+//
+//  Created by Steve Chakif on 2/16/14.
+//  Copyright (c) 2014 Steve Chakif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Practice1AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Practice1AppDelegate class]));
+    }
+}
